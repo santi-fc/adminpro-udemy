b960f59eea04779b69be40ab9a242d82281ea51c
SANTIAGO FAGÚNDEZ <oberkom@mc-Santi.local>
2018-02-07 16:37:00 +0100

    Primera carga

---
+++ src/app/login/register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  styleUrls : ['./login.component.css'],
+  templateUrl: './register.component.html'
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
